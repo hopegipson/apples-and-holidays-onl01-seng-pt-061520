5b81a51ab2465fb813b6c6c4c2d16364c3bb226d
Hope Gipson <gipson.hope@gmail.com>
2020-06-27 04:08:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,6 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
          return holiday if supply.include?"BBQ"
           
           end
+          holiday.flatten
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 end
