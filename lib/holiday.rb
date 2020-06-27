d2df3a7db37e0abdca00a49481fbf5dd141966f2
Hope Gipson <gipson.hope@gmail.com>
2020-06-27 03:49:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ stringseason = season.to_s
       puts "#{stringseason.capitalize}:"
       holiday.each do |holiday, supply|
        stringholiday = holiday.to_s.split("_").collect {|holly| holly.capitalize!}.join(" ")
-        puts "#{stringholiday}: #{supply}"
+        puts "#{stringholiday}: #{supply.join(}"
       end
       end
     
